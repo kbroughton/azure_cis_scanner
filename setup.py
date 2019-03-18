c26721b2ccd3da050325f97cc3b9f62d56118a3e
Kesten Broughton <kesten.broughton@praetorian.com>
2019-03-18 16:19:56 -0500

    [2019-03-18] 0.3.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 # Package info
 NAME = 'azure_cis_scanner'
 ROOT = os.path.dirname(__file__)
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 VERSION = __version__
 
 # Requirements
