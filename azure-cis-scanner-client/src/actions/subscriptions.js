ff9e3e9da9ef43fc1a2193f75486188d667e375d
Sonya Slegers <sonjay08@gmail.com>
2019-04-23 00:18:49 -0500

    deleted old getSubscription fetch call and related consts

--- azure-cis-scanner-client/src/actions/subscriptions.js
+++ azure-cis-scanner-client/src/actions/subscriptions.js
@@ -1,140 +1,108 @@
-import {normalizeResponseErrors} from './utils';
+import { normalizeResponseErrors } from "./utils";
 
-export const SET_SERVICE = 'SET_SERVICE';
+export const SET_SERVICE = "SET_SERVICE";
 export const setService = data => ({
-    type: SET_SERVICE,
-    data
-})
+  type: SET_SERVICE,
+  data
+});
 
-export const GET_SUBSCRIPTIONS_REQUEST = 'GET_SUBSCRIPTIONS_REQUEST';
+export const GET_SUBSCRIPTIONS_REQUEST = "GET_SUBSCRIPTIONS_REQUEST";
 export const getSubscriptionsRequest = data => ({
-    type: GET_SUBSCRIPTIONS_REQUEST,
-    data
+  type: GET_SUBSCRIPTIONS_REQUEST,
+  data
 });
 
-export const GET_SUBSCRIPTIONS_SUCCESS = 'GET_SUBSCRIPTIONS_SUCCESS';
+export const GET_SUBSCRIPTIONS_SUCCESS = "GET_SUBSCRIPTIONS_SUCCESS";
 export const getSubscriptionsSuccess = data => ({
-    type: GET_SUBSCRIPTIONS_SUCCESS,
-    data
+  type: GET_SUBSCRIPTIONS_SUCCESS,
+  data
 });
 
-export const GET_SUBSCRIPTIONS_ERROR = 'GET_SUBSCRIPTIONS_ERROR';
+export const GET_SUBSCRIPTIONS_ERROR = "GET_SUBSCRIPTIONS_ERROR";
 export const getSubscriptionsError = error => ({
-    type: GET_SUBSCRIPTIONS_ERROR,
-    error
+  type: GET_SUBSCRIPTIONS_ERROR,
+  error
 });
 
-// export const SELECT_SUBSCRIPTION_REQUEST = 'SELECT_SUBSCRIPTION_REQUEST';
-// export const selectSubscriptionRequest = error => ({
-//     type: SELECT_SUBSCRIPTION_REQUEST,
-//     error
-// });
-export const SELECT_SUBSCRIPTION = 'SELECT_SUBSCRIPTION';
+export const SELECT_SUBSCRIPTION = "SELECT_SUBSCRIPTION";
 export const selectSubscription = data => ({
-    type: SELECT_SUBSCRIPTION,
-    data
+  type: SELECT_SUBSCRIPTION,
+  data
 });
 
-// export const SELECT_SUBSCRIPTION_ERROR = 'SELECT_SUBSCRIPTION_ERROR';
-// export const selectSubscriptionError = error => ({
-//     type: SELECT_SUBSCRIPTION_ERROR,
-//     error
-// });
-
-export const SELECT_SERVICE_REQUEST = 'SELECT_SERVICE_REQUEST';
+export const SELECT_SERVICE_REQUEST = "SELECT_SERVICE_REQUEST";
 export const selectServiceRequest = error => ({
-    type: SELECT_SERVICE_REQUEST,
-    error
+  type: SELECT_SERVICE_REQUEST,
+  error
 });
-export const SELECT_SERVICE_SUCCESS = 'SELECT_SERVICE_SUCCESS';
+export const SELECT_SERVICE_SUCCESS = "SELECT_SERVICE_SUCCESS";
 export const selectServiceSuccess = data => ({
-    type: SELECT_SERVICE_SUCCESS,
-    data
+  type: SELECT_SERVICE_SUCCESS,
+  data
 });
 
-export const SELECT_SERVICE_ERROR = 'SELECT_SERVICE_ERROR';
+export const SELECT_SERVICE_ERROR = "SELECT_SERVICE_ERROR";
 export const selectServiceError = error => ({
-    type: SELECT_SERVICE_ERROR,
-    error
+  type: SELECT_SERVICE_ERROR,
+  error
 });
 
-export const GET_CIS_REQUEST = 'GET_CIS_REQUEST';
+export const GET_CIS_REQUEST = "GET_CIS_REQUEST";
 export const getCisRequest = error => ({
-    type: GET_CIS_REQUEST,
-    error
+  type: GET_CIS_REQUEST,
+  error
 });
-export const GET_CIS_SUCCESS = 'GET_CIS_SUCCESS';
+export const GET_CIS_SUCCESS = "GET_CIS_SUCCESS";
 export const getCisSuccess = data => ({
-    type: GET_CIS_SUCCESS,
-    data
+  type: GET_CIS_SUCCESS,
+  data
 });
 
-export const GET_CIS_ERROR = 'GET_CIS_ERROR';
+export const GET_CIS_ERROR = "GET_CIS_ERROR";
 export const getCisError = error => ({
-    type: GET_CIS_ERROR,
-    error
+  type: GET_CIS_ERROR,
+  error
 });
 
-export const getSubscriptions = () => (dispatch) => {
-    return fetch(`http://localhost:5000/subscription_tuples`, {
-        method: 'GET',
-        mode: 'cors'
+export const getSubscriptions = () => dispatch => {
+  return fetch(`http://localhost:5000/subscription_tuples`, {
+    method: "GET",
+    mode: "cors"
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(data => {
+      let dataArray = [""];
+      // create subscription dir from tuples
+      for (let i = 0; i < data.length; i++) {
+        let subscription_dir =
+          data[i][2].split(" ").join("-") + "-" + data[i][0].split("-")[0];
+        dataArray.push(subscription_dir);
+      }
+      dispatch(getSubscriptionsSuccess(dataArray));
     })
-        .then(res => normalizeResponseErrors(res))
-        .then(res => res.json())
-        .then(data => {
-            let dataArray = [""];
-            // create subscription dir from tuple
-            for (let i = 0; i < data.length; i++) {
-                let subscription_dir = data[i][2]
-                    .split(" ")
-                    .join("-") + "-" + data[i][0]
-                    .split("-")[0] 
-                dataArray.push(subscription_dir)
-            };
-            console.log("subscriptions = ", dataArray)
-            dispatch(getSubscriptionsSuccess(dataArray))})
-        .catch(err => dispatch(getSubscriptionsError(err)));
+    .catch(err => dispatch(getSubscriptionsError(err)));
 };
-
-// export const selectSubscription = (subscription) => (dispatch) => {
-//     console.log(subscription)
-//     return fetch(`http://localhost:5000/subscriptions/${subscription}`, {
-//         mode: 'cors',
-//         headers: {'Access-Control-Allow-Origin': '*'}
-//     })
-//         .then(res => normalizeResponseErrors(res))
-//         .then(res => res.json())
-//         .then(data =>  {
-//             console.log("data", data);
-//             dispatch(selectSubscriptionSuccess(data))
-//         })
-//         .catch(err => dispatch(selectSubscriptionError(err)));
-// }
 
 // get cis structure for nav
-export const getCis = () => (dispatch) => {
-    return fetch(`http://localhost:5000/cis_structure`, {
-        method: 'GET',
-        mode: 'cors'
+export const getCis = () => dispatch => {
+  return fetch(`http://localhost:5000/cis_structure`)
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(data => {
+      dispatch(getCisSuccess(data));
     })
-        .then(res => normalizeResponseErrors(res))
-        .then(res => res.json())
-        .then(data => {
-            dispatch(getCisSuccess(data))})
-        .catch(err => dispatch(getCisError(err)));
+    .catch(err => dispatch(getCisError(err)));
 };
 
-export const selectService = (service) => (dispatch) => {
-    console.log(service)
-    return fetch(`http://localhost:5000/services/${service}`, {
-        mode: 'cors',
-        headers: {'Access-Control-Allow-Origin': '*'}
+export const selectService = service => dispatch => {
+  console.log(service);
+  return fetch(`http://localhost:5000/services/${service}`)
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      dispatch(selectServiceSuccess(data));
     })
-        .then(res => normalizeResponseErrors(res))
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            dispatch(selectServiceSuccess(data))})
-        .catch(err => dispatch(selectServiceError(err)));
-}
+    .catch(err => dispatch(selectServiceError(err)));
+};
